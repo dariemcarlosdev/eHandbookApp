eb0428b0b2875a2c04bc0267a9a43806115f8e5f
Dariem Carlos Macias Mora <31046315+dariemcarlosdev@users.noreply.github.com>
2023-10-05 20:43:23 -0400

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -2,23 +2,23 @@ AZUREPAT=$AZUREPAT
 AZUSERNAME=$AZUSERNAME
 AZUSER_EMAIL=$AZUSER_EMAIL
 AZORG=$AZORG
-git clone https://github.com/dariemcarlosdev/eHandbookApp
-cd eHandbookApp
-rm -rf .git
 
-cd ..
+# Remove Git information (for fresh git start)
+rm -rf eHandbookApp/.git
 
-GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/eHandBook/_git/eHandbookapp"
-git clone $GIT_CMD_REPOSITORY
+# Fetch the changes from Azure DevOps to ensure we have latest
+git fetch --unshallow
 
-cp -r eHandbookApp/* eHandbookApp/
+# Pull changes from Azure DevOps if its exiting branch and have commits on it
+git pull https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/eHandBook/_git/eHandbookApp.git
 
-cd TestGitSync
+#git checkout -b $github_to_azure_sync
 
+# Set Git user identity
 git config --global user.email "$AZUSER_EMAIL"
 git config --global user.name "$AZUSERNAME"
 
+# Add all changes into stage, commit, and push to Azure DevOps
 git add .
-git commit -m "sync from git to azure"
-
-git push
+git commit -m "Sync from GitHub to Azure DevOps"
+git push --force https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/eHandBook/_git/eHandbookApp.git
